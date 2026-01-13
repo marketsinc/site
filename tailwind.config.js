4c6646b38d471b9fef2b29a0595e2a73129cf8db
decanus <7621705+decanus@users.noreply.github.com>
2026-01-13 10:36:15 +0100

    whoops

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_insights/**/*.md',
+    './*.html',
+    './*.md'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
